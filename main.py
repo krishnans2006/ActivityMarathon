0cb5e6ac3a40d50c916509346611c7ce94c42716
Krishnan Shankar <krishnans2006@gmail.com>
2020-10-08 23:59:58 +0000

    Moved data from CSV to ReplIt DB, added Upload and Shop pages with basic frontend functionality, fixed bugs.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-import csv
+from replit import db
+from datetime import timedelta
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "hello"
+app.config["SECRET_KEY"] = "fsediwq3e78fwshdore"
 
 
-def make_session_permanent():
+def set_session_timeout(remember):
     session.permanent = True
+    if remember:
+        app.permanent_session_lifetime = timedelta(days=20)
+    else:
+        app.permanent_session_lifetime = timedelta(minutes=20)
+    session.modified = True
+
+
+@app.before_first_request
+def populate_db():
+    if not db.get("accounts"):
+        db["accounts"] = []
 
 
 @app.route("/")
@@ -24,20 +36,17 @@ def login():
         email = request.form.get("email")
         pwd = request.form.get("password")
         rme = request.form.get("rememberme")
-        with open('data/accounts.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in reader:
-                if row[2] == email and row[3] == pwd:
-                    session["logged_in"] = True
-                    session["first_name"] = row[0]
-                    session["last_name"] = row[1]
-                    session["email"] = email
-                    session["pwd"] = pwd
-                    session["pts"] = row[4]
-                    flash(f"1Successfully Logged In as {row[0]} {row[1]}!")
-                    if rme:
-                        make_session_permanent()
-                    return redirect(url_for("home"))
+        for user in db["accounts"]:
+            if user["email"] == email and user["pwd"] == pwd:
+                session["logged_in"] = True
+                session["fname"] = user["fname"]
+                session["lname"] = user["lname"]
+                session["email"] = email
+                session["pwd"] = pwd
+                session["pts"] = user["pts"]
+                flash(f"1Successfully Logged In as {session['fname']} {session['lname']}!")
+                set_session_timeout(rme)
+                return redirect(url_for("home"))
         flash("0Incorrect Username or Password!")
         return redirect(url_for("login"))
     return render_template("login.html", session=session)
@@ -53,19 +62,15 @@ def register():
         lname = request.form.get("lname")
         email = request.form.get("email")
         pwd = request.form.get("password")
-        with open('data/accounts.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in reader:
-                if row[2] == email:
-                    flash(
+        for user in db["accounts"]:
+            if user["email"] == email:
+                flash(
                         "0You are already registered with this email! Please Log In or use another email."
                     )
-                    return redirect(url_for("register"))
-        with open('data/accounts.csv', 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow([fname, lname, email, pwd, 0])
-            flash("1Successfully Registered!")
-            return redirect(url_for("login"))
+                return redirect(url_for("register"))
+        db["accounts"] = db["accounts"] + [{"fname": fname, "lname": lname, "email": email, "pwd": pwd, "pts": 0}]
+        flash("1Successfully Registered!")
+        return redirect(url_for("login"))
     return render_template("register.html", session=session)
 
 
@@ -78,11 +83,23 @@ def logout():
 
 @app.route("/leaderboard")
 def leaderboard():
-    with open('data/accounts.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        sorted_by_pts = sorted(reader, key=lambda l: l[4], reverse=True)
+    sorted_by_pts = sorted(db["accounts"], key=lambda l: l["pts"], reverse=True)
     return render_template("leaderboard.html", leaderboard=sorted_by_pts)
 
 
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        file = request.files["file"]
+        flash("1File was sent to review successfully!")
+        return redirect(url_for("leaderboard"))
+    return render_template("upload.html")
+
+@app.route("/shop", methods=["GET", "POST"])
+def shop():
+    return render_template("shop.html")
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    
